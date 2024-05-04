343d629f683c88da7e33d2ccbc1549309b0ffa03
Igor Favarin <igor.w.c.favarin@gmail.com>
2024-05-04 20:49:26 -0300

    Resolução desafio de projeto conta bancaria

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome completo: ");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, insira o numero da sua conta");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Insira sua agencia: ");
+        String agencia = scanner.next();
+
+        double saldo = 50.0;
+
+        System.out.println("------------------------------------------------");
+        System.out.println("Olá "+ nome + ", obrigado por abrir sua conta conosco, sua agência é " + agencia + ", conta "+ numeroConta + " e seu saldo "+ saldo + " está disponível para saque.");
+
     }
 }
